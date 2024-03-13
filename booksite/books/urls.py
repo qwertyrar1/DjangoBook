a34b4658aed7bf03b4c9aed74d03d4554b4a22c9
qwertyrar1 <tyrsinartem@gmail.com>
2024-03-14 02:22:16 +0500

    added author page and search by books and authors

--- booksite/books/urls.py
+++ booksite/books/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'books'
 urlpatterns = [
-    path("", views.index, name="book"),
-    path("<int:book_id>/", views.detail, name="detail"),
+    path("", views.IndexView.as_view(), name="book"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("author/<int:author_id>", views.author, name="author"),
+    path("search/", views.search_feature, name="search-view"),
 ]
--- booksite/books/views.py
+++ booksite/books/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Book
+from django.core.paginator import Paginator
+from django.views import generic
+from .models import Book, Author
+from itertools import chain
 
 
-def index(request):
-    latest_book_list = Book.objects.order_by("-pub_date")[:5:-1]
+class IndexView(generic.ListView):
+    paginate_by = 4
+    model = Book
+    template_name = 'books/index.html'
+    context_object_name = 'latest_book_list'
+
+    def get_queryset(self):
+        return Book.objects.order_by("-pub_date")[::-1]
+
+
+class DetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/detail.html'
+
+
+def author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    author_books = Book.objects.filter(author_id=author_id)
     context = {
-        "latest_book_list": latest_book_list,
+        "author": author,
+        "author_books": author_books,
     }
-    return render(request, "books/index.html", context)
+    return render(request, 'books/author.html', context)
 
 
-def detail(request, book_id):
-    book = get_object_or_404(Book, pk=book_id)
-    return render(request, "books/detail.html", {"book": book})
+def search_feature(request):
+    if request.method == 'POST':
+        search_query = request.POST['search_query']
+
+        book_search = Book.objects.filter(name__icontains=search_query)
+        author_search_id = Author.objects.filter(fullname__icontains=search_query)
+        author_search = Book.objects.filter(author__in=author_search_id)
+        objects = list(chain(book_search, author_search))
+
+        page_number = request.GET.get("page")
+        paginator = Paginator(objects, 4)
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'books/index.html', {'query': search_query, 'page_obj': page_obj})
+    else:
+        return render(request, 'books/index.html', {})
+
